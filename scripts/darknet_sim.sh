74392a1a40f59c4711f6a67df4349d72f47b88e7
alessandro ianne <alessandro ianne>
2019-02-21 11:13:25 +0100

    added a python for the fault list generation

---
+++ scripts/darknet_sim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this script has been created in order to automatize the execution of a one simulation in darkent
+
+# initialize the variable that should be used during darknet simualation
+
+data="../cfg/mnist.dataset"
+network_configuration="../cfg/mnist_lenet.cfg"
+weight="../pretrained_weights/mnist_lenet.weights"
+test_set="../data/mnist_data/mnist/images/validation_images.txt"
+fault_list="../fault_list"
+
+echo "begin the simulation"
+
+# execute darknet 
+../darknet simulation $data $network_configuration $weight $test_set $fault_list -layer 0
+
