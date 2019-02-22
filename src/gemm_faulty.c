7d133f5974103838d5884ed21a9fde3a16dde620
alessandro ianne <alessandro ianne>
2019-02-22 12:12:36 +0100

    another injection test

--- src/gemm_faulty.c
+++ src/gemm_faulty.c
@@ -149,7 +149,11 @@ void gemm_nn_faulty_stuck_at(void *fault, int M, int N, int K, float ALPHA,
     
     stuck_at_fault f_parsed = *((stuck_at_fault *) fault);
     int mul_counter = f_parsed.fault_location;
-    int counter1 = 0; int counter2 = 0;
+
+    // only for debug
+    fprintf(stderr, "the fault injected is type %d, the position is %d, the bit is %d", f_parsed.type, f_parsed.fault_location, f_parsed.bit);
+
+    //int counter1 = 0; int counter2 = 0;
     /*
     printf("total number of multiplication %d\n", K * M * N);
     printf("the fault location is %d, the bit position is %d, the stuck-at type is %d \n", mul_counter, f_parsed.bit, f_parsed.type);*/
@@ -164,16 +168,16 @@ void gemm_nn_faulty_stuck_at(void *fault, int M, int N, int K, float ALPHA,
                 if (mul_counter == 1) {
                     float tmp2 = compute_faulty_multiplication(tmp, f_parsed.bit, f_parsed.type);
                     C[i*ldc+j] += tmp2;
-                    counter2++;
-                    printf("the correct result is %f the faulty one is %f\n", tmp, tmp2);
-                    printf("the counter for moltiplication is %d, the counter for the mac injection is %d\n", counter1, counter2);
+                    //counter2++;
+                    //printf("the correct result is %f the faulty one is %f\n", tmp, tmp2);
+                    //printf("the counter for moltiplication is %d, the counter for the mac injection is %d\n", counter1, counter2);
                     mul_counter = MAC_UNIT_N;
                 } else {
                     C[i*ldc+j] += tmp;
                 }
                 //C[i*ldc+j] += A_PART*B[k*ldb+j];   
-                mul_counter--;
-                counter1++;
+               // mul_counter--;
+               // counter1++;
             }
         }
     }
