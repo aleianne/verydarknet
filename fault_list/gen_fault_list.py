38f7dcc814f8e15e5fafeab5161f5f539fff971b
alessandro ianne <alessandro ianne>
2019-02-25 17:26:47 +0100

    fixed a minor bug

--- fault_list/gen_fault_list.py
+++ fault_list/gen_fault_list.py
@@ -15,6 +15,7 @@ def generate_stuck_at_fault_list(fault_list_name, component_num):
     print("begin to create the fault list file")
 
     with open(fault_list_name, "w") as fp: 
+        
         for i in range(component_num): 
             for j in range(23, 31):
                 # generate the string to be insert into the fault list file
